1c2cc2bfcee7930df0b216ef232f92396d23c8a8
Hanisyauni <148435216+Hanisyauni@users.noreply.github.com>
2023-10-20 13:24:23 +0800

    Update project.py

--- project.py
+++ project.py
@@ -11,12 +11,13 @@ This app predicts the **Advertising Sale** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    Tv = st.sidebar.slider('Tv ', 10.0, 235.0, 17.2)
+    Tv = st.sidebar.slider('Tv', 10.0, 235.0, 17.2)
     Radio = st.sidebar.slider('Radio', 10.0, 46.0, 39.3)
     Newspaper = st.sidebar.slider('Newspaper', 40.0, 70.0, 69.3)
     data = {'Tv': Tv,
             'Radio': Radio,
             'Newspaper': Newspaper}
+    
     features = pd.DataFrame(data, index=[0])
     return features
 
